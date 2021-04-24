4eb10c0984f784daadb09436ffc27dda86d124d4
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-04-24 08:55:28 +0530

    Add base biolerplate code in react

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 python manage.py migrate
+python manage.py collectstatic --no-input
 gunicorn -w 3 -b :8000 backend.config.wsgi:application
---
+++ src/components/Videos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Videos = (props) => {
+  return <></>;
+};
+
+export default Videos;
