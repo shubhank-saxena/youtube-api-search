fc70850ff27ee7f160cfd55f177811545131a5a7
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2021-05-12 10:26:52 +0530

    correct asgi and wsgi

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,6 +1,4 @@
 """
-ASGI config for fampay project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
@@ -11,6 +9,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fampay.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
 
 application = get_asgi_application()
